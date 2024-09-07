5efdd16431254a44fb8c469747a5e63c89a110c9
Toraenor <olivier.maurin.development@gmail.com>
2024-09-07 15:17:00 +0200

    Update SilverAmethystRing.cs

--- Netherveil/Assets/WorkAssets/Code/Items/PassiveItems/SilverAmethystRing.cs
+++ Netherveil/Assets/WorkAssets/Code/Items/PassiveItems/SilverAmethystRing.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+//Copyright 2024 Property of Olivier Maurin.All rights reserved.
 public class SilverAmethystRing : ItemEffect, IPassiveItem
 {
     private readonly int maxLifeStat = 25;
